9eac08138a6713da8f91b4216288f13671a7d7dc
tianjiahe <390389348@qq.com>
2020-11-06 01:12:14 +0800

    主页蓝屏测试

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //color: Color.fromRGBO(100, 100, 000, 0.9),
+      color: Colors.blue,
+    );
+  }
+}
